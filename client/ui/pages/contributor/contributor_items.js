8312767b9bd9b0a6239cc5aca02d1a840d908adb
austinsand <austinsand@gmail.com>
2018-01-01 08:29:53 -0500

    Added a page for showing items associated with a contributor

---
+++ client/ui/pages/contributor/contributor_items.js
@@ -0,0 +1,33 @@
+import './contributor_items.html';
+import { Template } from 'meteor/templating';
+
+/**
+ * Template Helpers
+ */
+Template.ContributorItems.helpers({});
+
+/**
+ * Template Event Handlers
+ */
+Template.ContributorItems.events({});
+
+/**
+ * Template Created
+ */
+Template.ContributorItems.onCreated(() => {
+  
+});
+
+/**
+ * Template Rendered
+ */
+Template.ContributorItems.onRendered(() => {
+  
+});
+
+/**
+ * Template Destroyed
+ */
+Template.ContributorItems.onDestroyed(() => {
+  
+});
