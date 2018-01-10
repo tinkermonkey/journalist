d6b93ee21b3a1ec52bb42c83c0698d22a5dd87ec
Austin Sand <austinsand@gmail.com>
2018-01-10 10:28:57 -0500

    Added dashboard metric template

---
+++ client/ui/components/charts/dashboard_metric.js
@@ -0,0 +1,32 @@
+import './dashboard_metric.html';
+import './dashboard_metric.css';
+import { Template } from 'meteor/templating';
+
+/**
+ * Template Helpers
+ */
+Template.DashboardMetric.helpers({});
+
+/**
+ * Template Event Handlers
+ */
+Template.DashboardMetric.events({});
+
+/**
+ * Template Created
+ */
+Template.DashboardMetric.onCreated(() => {
+});
+
+/**
+ * Template Rendered
+ */
+Template.DashboardMetric.onRendered(() => {
+});
+
+/**
+ * Template Destroyed
+ */
+Template.DashboardMetric.onDestroyed(() => {
+  
+});
