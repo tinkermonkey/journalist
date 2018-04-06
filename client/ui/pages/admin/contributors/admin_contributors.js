68da8366453e47c0ce1ad08ec4ee364212c3c78b
Austin Sand <austinsand@gmail.com>
2018-04-06 08:51:17 -0400

    Added ability to create users from contributors easily

--- client/ui/pages/admin/contributors/admin_contributors.js
+++ client/ui/pages/admin/contributors/admin_contributors.js
@@ -4,6 +4,8 @@ import SimpleSchema                   from 'simpl-schema';
 import { RobaDialog }                 from 'meteor/austinsand:roba-dialog';
 import { Contributors }               from '../../../../../imports/api/contributors/contributors.js';
 import { ContributorRoleDefinitions } from '../../../../../imports/api/contributors/contributor_role_definitions.js';
+import { UserTypes }                  from '../../../../../imports/api/users/user_types';
+import { Util }                       from '../../../../../imports/api/util';
 import '../../../components/add_record_form/add_record_form.js';
 import '../../../components/team_roles/editable_team_roster.js';
 import './admin_contributors_table';
@@ -91,6 +93,79 @@ Template.AdminContributors.events({
           return;
         }
         RobaDialog.hide();
+      }.bind(this)
+    });
+  },
+  'click .btn-add-user' (e, instance) {
+    let context     = Template.currentData(),
+        contributor = this;
+    
+    RobaDialog.show({
+      contentTemplate: 'AddRecordForm',
+      contentData    : {
+        schema: new SimpleSchema({
+          email   : {
+            type    : String,
+            regEx   : SimpleSchema.RegEx.Email,
+            label   : 'Email',
+            autoform: {
+              type : 'hidden',
+              value: contributor.email
+            }
+          },
+          name    : {
+            type    : String,
+            optional: true,
+            label   : 'Name',
+            autoform: {
+              type : 'hidden',
+              value: contributor.name
+            }
+          },
+          password: {
+            type : String,
+            label: 'Password'
+          },
+          usertype: {
+            type    : Number,
+            label   : 'User Type',
+            autoform: {
+              options: _.keys(UserTypes).map((key) => {
+                return { label: Util.camelToTitle(key), value: UserTypes[ key ] }
+              })
+            }
+          }
+        })
+      },
+      title          : 'Add User',
+      width          : 400,
+      buttons        : [
+        { text: 'Cancel' },
+        { text: 'Add' }
+      ],
+      callback       : function (btn) {
+        if (btn.match(/add/i)) {
+          let formId = 'addRecordForm';
+          if (AutoForm.validateForm(formId)) {
+            let formData = AutoForm.getFormValues(formId).insertDoc;
+            
+            // Set the usertype
+            console.log('Add user data:', formData);
+            
+            // Create the user
+            Meteor.call('addUser', formData, (error, response) => {
+              if (error) {
+                RobaDialog.error('Failed to create user:' + error.toString())
+              } else {
+                RobaDialog.hide();
+              }
+            });
+            
+            AutoForm.resetForm(formId)
+          }
+        } else {
+          RobaDialog.hide();
+        }
       }.bind(this)
     });
   },
