1917ed8d6da01f537544ff662451d621caa26678
Austin Sand <austinsand@gmail.com>
2018-04-07 13:41:15 -0400

    Created a collection for creating configured collections

---
+++ imports/api/dynamic_collections/dynamic_collections.js
@@ -0,0 +1,73 @@
+import { Mongo }         from 'meteor/mongo';
+import SimpleSchema      from 'simpl-schema';
+import { SchemaHelpers } from '../schema_helpers';
+
+/**
+ * ============================================================================
+ * DynamicCollections
+ * ============================================================================
+ */
+export const DynamicCollection = new SimpleSchema({
+  title           : {
+    type: String
+  },
+  preambleCode    : {
+    type    : String,
+    optional: true
+  },
+  schemaCode      : {
+    type    : String,
+    optional: true
+  },
+  helpersCode     : {
+    type    : String,
+    optional: true
+  },
+  methodsCode     : {
+    type    : String,
+    optional: true
+  },
+  publicationsCode: {
+    type    : String,
+    optional: true
+  },
+  // Standard tracking fields
+  dateCreated     : {
+    type     : Date,
+    autoValue: SchemaHelpers.autoValueDateCreated
+  },
+  createdBy       : {
+    type     : String,
+    autoValue: SchemaHelpers.autoValueCreatedBy
+  },
+  dateModified    : {
+    type     : Date,
+    autoValue: SchemaHelpers.autoValueDateModified
+  },
+  modifiedBy      : {
+    type     : String,
+    autoValue: SchemaHelpers.autoValueModifiedBy
+  }
+});
+
+export const DynamicCollections = new Mongo.Collection("dynamic_collections");
+DynamicCollections.attachSchema(DynamicCollection);
+ChangeTracker.trackChanges(DynamicCollections, 'DynamicCollections');
+
+// These are server side only
+DynamicCollections.deny({
+  remove () {
+    return true;
+  },
+  insert () {
+    return true;
+  },
+  update () {
+    return true;
+  }
+});
+
+/**
+ * Helpers
+ */
+DynamicCollections.helpers({});
