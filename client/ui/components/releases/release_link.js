b1a7846a6040762ff1ec9b06c390c1e8a07c74b9
Austin Sand <austinsand@gmail.com>
2018-04-11 09:29:12 -0400

    Added basic release link

---
+++ client/ui/components/releases/release_link.js
@@ -0,0 +1,33 @@
+import './release_link.html';
+import { Template } from 'meteor/templating';
+
+/**
+ * Template Helpers
+ */
+Template.ReleaseLink.helpers({});
+
+/**
+ * Template Event Handlers
+ */
+Template.ReleaseLink.events({});
+
+/**
+ * Template Created
+ */
+Template.ReleaseLink.onCreated(() => {
+  
+});
+
+/**
+ * Template Rendered
+ */
+Template.ReleaseLink.onRendered(() => {
+  
+});
+
+/**
+ * Template Destroyed
+ */
+Template.ReleaseLink.onDestroyed(() => {
+  
+});
