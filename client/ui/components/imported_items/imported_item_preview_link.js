afc75cbaa5c2846d2e2cf3e59e5519f914ed2ffc
Austin Sand <austinsand@gmail.com>
2018-04-16 07:27:14 -0400

    Updated preview link to just load the linked item to avoid subscription problems

--- client/ui/components/imported_items/imported_item_preview_link.js
+++ client/ui/components/imported_items/imported_item_preview_link.js
@@ -1,9 +1,8 @@
 import './imported_item_preview_link.html';
-import { Template }           from 'meteor/templating';
+import { Template }      from 'meteor/templating';
 import '../quick_popover/quick_popover';
 import './imported_item_preview';
-import { ImportedItemCrumbs } from '../../../../imports/api/imported_items/imported_item_crumbs';
-import { ImportedItems }      from '../../../../imports/api/imported_items/imported_items';
+import { ImportedItems } from '../../../../imports/api/imported_items/imported_items';
 
 /**
  * Template Helpers
@@ -13,16 +12,9 @@ Template.ImportedItemPreviewLink.helpers({
    * Grab something with the title and identifier for display purposes
    */
   itemCrumb () {
-    let itemId = this.toString(),
-        crumb  = ImportedItemCrumbs.findOne(itemId);
-    if (crumb) {
-      //console.log('ImportedItemPreviewLink.itemCrumb:', itemId, crumb);
-      return crumb
-    } else {
-      //let item = ImportedItems.findOne(itemId);
-      //console.log('ImportedItemPreviewLink.itemCrumb:', itemId, item);
-      return ImportedItems.findOne(itemId);
-    }
+    let itemId = this.toString();
+    
+    return ImportedItems.findOne(itemId);
   },
   previewTemplate () {
     return Template.ImportedItemPreview
@@ -38,6 +30,13 @@ Template.ImportedItemPreviewLink.events({});
  * Template Created
  */
 Template.ImportedItemPreviewLink.onCreated(() => {
+  let instance = Template.instance();
+  
+  instance.autorun(() => {
+    let itemId = Template.currentData().toString();
+    
+    instance.subscribe('imported_item', itemId);
+  })
 });
 
 /**
