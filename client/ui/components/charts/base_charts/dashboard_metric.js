37c4f1e363011935223c1d91c6b32cf52c9b8ee9
Austin Sand <austinsand@gmail.com>
2018-06-18 09:25:50 -0400

    Updated Dashboard Metric styling

--- client/ui/components/charts/base_charts/dashboard_metric.js
+++ client/ui/components/charts/base_charts/dashboard_metric.js
@@ -2,10 +2,36 @@ import './dashboard_metric.html';
 import './dashboard_metric.css';
 import { Template } from 'meteor/templating';
 
+let d3 = require('d3');
+
+d3.scale = require('d3-scale');
+
 /**
  * Template Helpers
  */
-Template.DashboardMetric.helpers({});
+Template.DashboardMetric.helpers({
+  color () {
+    let context     = this,
+        customScale = Template.instance().customScale.get();
+    
+    if (customScale && context.customColorScale) {
+      try {
+        let numericValue = parseFloat(context.value),
+            colorIndex   = parseInt((numericValue - context.customColorScale.min) / (context.customColorScale.max - context.customColorScale.min) * (context.customColorScale.max - context.customColorScale.min));
+        
+        console.log('color:', numericValue, colorIndex, ((numericValue - context.customColorScale.min) / (context.customColorScale.max - context.customColorScale.min)) * (context.customColorScale.max - context.customColorScale.min));
+        for(let i = 0; i < (context.customColorScale.max - context.customColorScale.min) * 2; i++){
+          console.log('DashboardMetric.color:', i, customScale(i));
+        }
+        return customScale(colorIndex)
+      } catch (e) {
+        console.error('DashboardMetric.color failed:', e);
+      }
+    } else {
+      return 'transparent'
+    }
+  }
+});
 
 /**
  * Template Event Handlers
@@ -16,6 +42,31 @@ Template.DashboardMetric.events({});
  * Template Created
  */
 Template.DashboardMetric.onCreated(() => {
+  let instance = Template.instance();
+  
+  instance.customScale = new ReactiveVar();
+  
+  instance.autorun(() => {
+    let context = Template.currentData();
+    
+    if (context && context.customColorScale && context.customColorScale.colors) {
+      try {
+        let scale = d3.scale.scaleLinear().domain([ 1, Math.max(context.customColorScale.max - context.customColorScale.min, context.customColorScale.colors.length) ])
+            .interpolate(d3.interpolateHcl)
+            .range(context.customColorScale.colors.map((d) => {
+              console.log('d3 custom color scale:', d, context.customColorScale.colors.length);
+              return d3.rgb(d)
+            }));
+        
+        instance.customScale.set(scale);
+      } catch (e) {
+        console.error('DashboardMetric.autorun failed to calculate color scale:', e);
+        instance.customScale.set(null);
+      }
+    } else {
+      instance.customScale.set(null);
+    }
+  });
 });
 
 /**
